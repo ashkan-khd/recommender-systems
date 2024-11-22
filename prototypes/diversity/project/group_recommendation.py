95341f909d542750ee59ec5a24c1389562dc026b
Ashkan Khademian <ashkan.khd.q@gmail.com>
2024-11-22 23:03:34 +0200

    fix: fix predictor bugs

--- prototypes/diversity/project/group_recommendation.py
+++ prototypes/diversity/project/group_recommendation.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 from functools import lru_cache
 from typing import List
 
@@ -8,11 +9,17 @@ from prototypes.diversity.project.constants import *
 from prototypes.diversity.project.group_preference_lists import GroupPreferenceLists
 
 
-class GroupRecommendationInterface:
-    pass
+class GroupRecommendationAnalyzerInterface(ABC):
+    @abstractmethod
+    def get_user_group_recommendation_satisfaction(self, user_id: UserIDType) -> float:
+        pass
+
+    @abstractmethod
+    def get_group_satisfaction(self) -> float:
+        pass
 
 
-class GroupRecommendationAnalyzer:
+class GroupRecommendationAnalyzer(GroupRecommendationAnalyzerInterface):
 
     def __init__(
             self,
@@ -26,17 +33,17 @@ class GroupRecommendationAnalyzer:
         self.group_preference_lists = group_preference_lists
         self.preference_score_calculator = preference_score_calculator
 
-    def __get_user_group_list_satisfaction(self, user_id: UserIDType):
+    def __get_user_group_list_satisfaction(self, user_id: UserIDType) -> float:
         return sum(
             self.preference_score_calculator(user_id, movie_id)
             for movie_id in self.group_recommendation
         )
 
-    def __get_user_average_group_list_satisfaction(self, user_id: UserIDType):
+    def __get_user_average_group_list_satisfaction(self, user_id: UserIDType) -> float:
         return self.__get_user_group_list_satisfaction(user_id) / len(self.group_recommendation)
 
     @lru_cache(maxsize=None)
-    def get_user_group_recommendation_satisfaction(self, user_id: UserIDType):
+    def get_user_group_recommendation_satisfaction(self, user_id: UserIDType) -> float:
         return min(
             self.__get_user_group_list_satisfaction(
                 user_id
@@ -44,38 +51,38 @@ class GroupRecommendationAnalyzer:
             1.
         )
 
-    def get_group_satisfaction(self):
+    def get_group_satisfaction(self) -> float:
         return sum(
             self.get_user_group_recommendation_satisfaction(user_id)
             for user_id in self.group
         ) / len(self.group)
 
-    def get_group_disagreement(self):
+    def get_group_disagreement(self) -> float:
         scores = [self.get_user_group_recommendation_satisfaction(user_id) for user_id in self.group]
         return max(scores) - min(scores)
 
-    def get_group_diversity(self):
+    def get_group_diversity(self) -> float:
         diversity_score_calculator = DiversityScoreCalculator()
         return diversity_score_calculator.calculate_list_diversity(self.group_recommendation)
 
 
-class GroupRecommendationsOverallAnalyzer:
+class GroupRecommendationsOverallAnalyzer(GroupRecommendationAnalyzerInterface):
     def __init__(
             self, group
     ):
         self.group = group
         self.group_recommendation_analyzers = []
 
-    def add_group_recommendation(self, group_recommendation_analyzer: GroupRecommendationAnalyzer):
+    def add_group_recommendation(self, group_recommendation_analyzer: GroupRecommendationAnalyzer) -> None:
         self.group_recommendation_analyzers.append(group_recommendation_analyzer)
 
-    def get_user_group_recommendation_satisfaction(self, user_id):
+    def get_user_group_recommendation_satisfaction(self, user_id) -> float:
         return sum(
             group_recommendation_analyzer.get_user_group_recommendation_satisfaction(user_id)
             for group_recommendation_analyzer in self.group_recommendation_analyzers
         ) / len(self.group_recommendation_analyzers)
 
-    def get_group_satisfaction(self):
+    def get_group_satisfaction(self) -> float:
         return sum(
             self.get_user_group_recommendation_satisfaction(user_id)
             for user_id in self.group
@@ -84,7 +91,7 @@ class GroupRecommendationsOverallAnalyzer:
     def get_last_group_recommendation(self) -> GroupRecommendationAnalyzer:
         return self.group_recommendation_analyzers[-1]
 
-    def get_diversity_between_last_two_group_recommendations(self):
+    def get_diversity_between_last_two_group_recommendations(self) -> float:
         assert len(self.group_recommendation_analyzers) >= 2
         last_group_recommendation = self.group_recommendation_analyzers[-1]
         previous_group_recommendation = self.group_recommendation_analyzers[-2]
@@ -92,5 +99,5 @@ class GroupRecommendationsOverallAnalyzer:
             last_group_recommendation.group_recommendation, previous_group_recommendation.group_recommendation
         )
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.group_recommendation_analyzers)
